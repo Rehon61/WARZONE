e062634f70e3dcdec5a0a1bfccf77c814ac4fff8
Никита <titan98@bk.ru>
2024-09-10 21:32:02 +0300

    gotovo4

--- news_warzone/models.py
+++ news_warzone/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
 class News(models.Model):
-    # title - заголовок
-    # DateField()
-    # TimeField()
+
     title = models.CharField(max_length=20)
     date = models.DateField()
     time = models.TimeField()
